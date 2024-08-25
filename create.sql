a5b5d77337dc9b5bbc883fa422ae3824c0dc7b25
Cheulong Sear <searcheulong@gmail.com>
2024-08-25 14:37:33 +0700

    Create shops table

--- create.sql
+++ create.sql
@@ -11,3 +11,10 @@ CREATE TABLE employees (
     salary INT,
     coffeeshop_id INT
 );
+
+-- Create shops table
+CREATE TABLE shops (
+    coffeeshop_id INT PRIMARY KEY,
+    coffeeshop_name VARCHAR(50),
+    city_id INT
+);
