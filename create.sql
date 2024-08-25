a7a72c667d37f522645ebbe8d6fb41d29c565808
Cheulong Sear <searcheulong@gmail.com>
2024-08-25 14:37:15 +0700

    Create employees table

---
+++ create.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLES ========================================
+
+-- Create employees table 
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    hire_date DATE,
+    gender VARCHAR(1), -- "M"/"F" (male/female)
+    salary INT,
+    coffeeshop_id INT
+);
